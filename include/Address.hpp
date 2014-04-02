8753da0bb717de5374e6b6b03f989f510f593a18
joaopapereira <joaopapereira@gmail.com>
2014-04-02 10:42:39 +0200

    Added comments for user and address

--- include/Address.hpp
+++ include/Address.hpp
@@ -11,16 +11,35 @@
 
 class Address{
 public:
+	/**
+	 * Class constructor
+	 * @param host Hostname or IP used to identify the address
+	 * @param port Port of the address
+	 */
 	Address(std::string host, int port): host(host), port(port){};
+	/**
+	 * Retrieve the host of the current address
+	 * @return Host
+	 */
 	std::string getHost(){
 		return host;
 	};
+	/**
+	 * Retrieve the port of the current address
+	 * @return Port number
+	 */
 	int getPort(){
 		return port;
 	};
 
 private:
+	/**
+	 * Hostname or IP of the address
+	 */
 	std::string host;
+	/**
+	 * Port number of the address
+	 */
 	int port;
 };
 
--- include/User.hpp
+++ include/User.hpp
@@ -10,15 +10,34 @@
 
 class User{
 public:
+	/**
+	 * Class constructor
+	 * @param username Username to use why connecting
+	 * @param password Password of the user
+	 */
 	User(std::string username, std::string password): username(username), password(password){};
+	/**
+	 * Retrieve the username
+	 * @return Username
+	 */
 	std::string getUsername(){
 		return username;
 	};
+	/**
+	 * Retrieve the password for the user
+	 * @return Password
+	 */
 	std::string getPassword(){
 		return password;
 	};
 private:
+	/**
+	 * Username
+	 */
 	std::string username;
+	/**
+	 * Password
+	 */
 	std::string password;
 };
 
