6c1daab55a12894b6397b321df6ff114fd36487d
joaopapereira <joaopapereira@gmail.com>
2014-05-20 18:58:01 +0200

    Add missing module

---
+++ include/Module.hpp
@@ -0,0 +1,27 @@
+/*
+ * Module.hpp
+ *
+ *  Created on: May 5, 2014
+ *      Author: joao
+ */
+
+#ifndef MODULE_HPP_
+#define MODULE_HPP_
+#include <string>
+#include <stdarg.h>
+#include "libJPLogger.hpp"
+class Module{
+public:
+	void log(int logsev, int type,std::string message , ...){
+		va_list args;
+		va_start( args, message );
+		jpCppLibs::OneInstanceLogger::instance().log(LOGNAME, logsev, type, args);
+		va_end( args );
+	};
+protected:
+	std::string LOGNAME;
+};
+
+
+
+#endif /* MODULE_HPP_ */
